b7e6f97d11b9fabb9cf35198307177b55034180d
Maxwell Pray <synthead@gmail.com>
2022-09-03 20:16:06 -0700

    Add phone_chars_for to CharEncoder.

--- lib/timex_datalink_client/char_encoder.rb
+++ lib/timex_datalink_client/char_encoder.rb
@@ -5,9 +5,12 @@ class TimexDatalinkClient
     CHARS = "0123456789abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:\\;=@?ABCDEF"
     EEPROM_TERMINATOR = 0x3f
 
-    def chars_for(string_chars)
+    PHONE_CHARS = "0123456789cfhpw "
+    PHONE_DIGITS = 12
+
+    def chars_for(string_chars, char_map: CHARS)
       string_chars.each_char.map do |string_char|
-        CHARS.index(string_char)
+        char_map.index(string_char)
       end
     end
 
@@ -16,6 +19,19 @@ class TimexDatalinkClient
 
       packed_int = chars.each_with_index.sum do |char, index|
         char << (6 * index)
+      end
+
+      packed_int.digits(256)
+    end
+
+    def phone_chars_for(string_chars)
+      padded_string_chars = string_chars.rjust(PHONE_DIGITS)
+      truncated_string_chars = padded_string_chars[0..PHONE_DIGITS - 1]
+
+      chars = chars_for(truncated_string_chars, char_map: PHONE_CHARS)
+
+      packed_int = chars.each_with_index.sum do |char, index|
+        char << (4 * index)
       end
 
       packed_int.digits(256)

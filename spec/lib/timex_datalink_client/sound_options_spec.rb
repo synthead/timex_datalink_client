3588304ce395e109e8296b4965efbc465ef59067
Maxwell Pray <synthead@gmail.com>
2022-09-04 19:15:56 -0700

    Use common lets in SoundOptions specs.

--- spec/lib/timex_datalink_client/sound_options_spec.rb
+++ spec/lib/timex_datalink_client/sound_options_spec.rb
@@ -3,6 +3,9 @@
 require "spec_helper"
 
 describe TimexDatalinkClient::SoundOptions do
+  let(:hourly_chimes) { 0 }
+  let(:button_beep) { 0 }
+
   let(:sound_options) do
     described_class.new(
       hourly_chimes: hourly_chimes,
@@ -13,22 +16,15 @@ describe TimexDatalinkClient::SoundOptions do
   describe "#packets", :crc do
     subject(:packets) { sound_options.packets }
 
-    context "when no sounds are enabled" do
-      let(:hourly_chimes) { 0 }
-      let(:button_beep) { 0 }
-
-      it_behaves_like "CRC-wrapped packets", [[0x71, 0x00, 0x00]]
-    end
+    it_behaves_like "CRC-wrapped packets", [[0x71, 0x00, 0x00]]
 
     context "when hourly chimes are enabled" do
       let(:hourly_chimes) { 1 }
-      let(:button_beep) { 0 }
 
       it_behaves_like "CRC-wrapped packets", [[0x71, 0x01, 0x00]]
     end
 
     context "when button beep is enabled" do
-      let(:hourly_chimes) { 0 }
       let(:button_beep) { 1 }
 
       it_behaves_like "CRC-wrapped packets", [[0x71, 0x00, 0x01]]

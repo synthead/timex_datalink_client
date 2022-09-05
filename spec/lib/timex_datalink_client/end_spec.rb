b7b69ec80af9663d137da5b24f8ea49e88aa37d1
Maxwell Pray <synthead@gmail.com>
2022-09-04 19:02:40 -0700

    Add End specs.

---
+++ spec/lib/timex_datalink_client/end_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe TimexDatalinkClient::End do
+  let(:end_instance) { described_class.new }
+
+  describe "#packets", :crc do
+    subject(:packets) { end_instance.packets }
+
+    it_behaves_like "CRC-wrapped packets", [[0x21]]
+  end
+end
+

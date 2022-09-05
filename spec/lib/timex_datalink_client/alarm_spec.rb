0010e83ce72add7322bf56b968fc2e42f5932d1e
Maxwell Pray <synthead@gmail.com>
2022-09-04 18:41:11 -0700

    Add Alarm specs.

---
+++ spec/lib/timex_datalink_client/alarm_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe TimexDatalinkClient::Alarm do
+  let(:number) { 1 }
+  let(:audible) { 0 }
+  let(:time) { Time.new(1994) }
+  let(:message) { "alarm 1" }
+
+  let(:alarm) do
+    described_class.new(
+      number: number,
+      audible: audible,
+      time: time,
+      message: message
+    )
+  end
+
+  describe "#packets", :crc do
+    subject(:packets) { alarm.packets }
+
+    it_behaves_like "CRC-wrapped packets", [
+      [0x50, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x00]
+    ]
+
+    context "when number is 2" do
+      let(:number) { 2 }
+
+      it_behaves_like "CRC-wrapped packets", [
+        [0x50, 0x02, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x00]
+      ]
+    end
+
+    context "when audible is 1" do
+      let(:audible) { 1 }
+
+      it_behaves_like "CRC-wrapped packets", [
+        [0x50, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x01]
+      ]
+    end
+
+    context "when time is 16:35" do
+      let(:time) { Time.new(1994, 1, 1, 16, 35) }
+
+      it_behaves_like "CRC-wrapped packets", [
+        [0x50, 0x01, 0x10, 0x23, 0x00, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x00]
+      ]
+    end
+
+    context "when message is \"wake up\"" do
+      let(:message) { "wake up" }
+
+      it_behaves_like "CRC-wrapped packets", [
+        [0x50, 0x01, 0x00, 0x00, 0x00, 0x00, 0x20, 0x0a, 0x14, 0x0e, 0x24, 0x1e, 0x19, 0x24, 0x00]
+      ]
+    end
+  end
+end

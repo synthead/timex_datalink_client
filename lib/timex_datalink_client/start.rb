8ea892b6929f33f515b667fd3eb32045fd702c1f
Maxwell Pray <synthead@gmail.com>
2022-09-08 00:41:50 -0700

    Add YARDoc comments to Start.

--- lib/timex_datalink_client/start.rb
+++ lib/timex_datalink_client/start.rb
@@ -8,6 +8,9 @@ class TimexDatalinkClient
 
     CPACKET_START = [0x20, 0x00, 0x00, 0x03]
 
+    # Compile packets for data start command.
+    #
+    # @return [Array<Array<Integer>>] Two-dimensional array of integers that represent bytes.
     def packets
       [CPACKET_START]
     end

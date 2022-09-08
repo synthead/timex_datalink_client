3426afb206ead983be78e762c97e3334dd1f2666
Maxwell Pray <synthead@gmail.com>
2022-09-07 23:09:30 -0700

    Accept raw sound theme data and SPC files in SoundTheme.

--- lib/timex_datalink_client/sound_theme.rb
+++ lib/timex_datalink_client/sound_theme.rb
@@ -14,10 +14,11 @@ class TimexDatalinkClient
 
     SOUND_DATA_HEADER = "\x25\x04\x19\x69"
 
-    attr_accessor :sound_theme_data
+    attr_accessor :spc_file
 
-    def initialize(sound_theme_data:)
+    def initialize(sound_theme_data: nil, spc_file: nil)
       @sound_theme_data = sound_theme_data
+      @spc_file = spc_file
     end
 
     def packets
@@ -26,20 +27,28 @@ class TimexDatalinkClient
 
     private
 
-    def sound_bytes
-      sound_theme_data.delete_prefix(SOUND_DATA_HEADER).bytes
-    end
-
     def load_sect
       CPACKET_SECT + [payloads.length, offset]
     end
 
     def payloads
-      paginate_cpackets(header: CPACKET_DATA, cpackets: sound_bytes)
+      paginate_cpackets(header: CPACKET_DATA, cpackets: sound_theme_data.bytes)
+    end
+
+    def sound_theme_data
+      @sound_theme_data || spc_file_data_without_header
+    end
+
+    def spc_file_data
+      File.open(spc_file, "rb").read
+    end
+
+    def spc_file_data_without_header
+      spc_file_data.delete_prefix(SOUND_DATA_HEADER)
     end
 
     def offset
-      0x100 - sound_bytes.length
+      0x100 - sound_theme_data.bytesize
     end
   end
 end

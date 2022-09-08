7500a57d36de7f4bcf048b4d220f486e9181860c
Maxwell Pray <synthead@gmail.com>
2022-09-08 00:11:04 -0700

    Add YARDoc comments to Eeprom::Appointment.

--- lib/timex_datalink_client/eeprom/appointment.rb
+++ lib/timex_datalink_client/eeprom/appointment.rb
@@ -11,11 +11,19 @@ class TimexDatalinkClient
 
       attr_accessor :time, :message
 
+      # Create an Appointment instance.
+      #
+      # @param time [Time] Time of appointment.
+      # @param message [String] Appointment text.
+      # @return [void]
       def initialize(time:, message:)
         @time = time
         @message = message
       end
 
+      # Compile a packet for an appointment.
+      #
+      # @return [Array<Integer>] Array of integers that represent bytes.
       def packet
         [
           time.month,

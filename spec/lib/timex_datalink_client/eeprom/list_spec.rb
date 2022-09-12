547cb4e3f900b60251d011a2876b19fd02b7d5f9
Maxwell Pray <synthead@gmail.com>
2022-09-11 22:31:53 -0700

    Move Protocol3::Eeprom specs to spec/lib/timex_datalink_client/eeprom.

--- spec/lib/timex_datalink_client/eeprom/list_spec.rb
+++ spec/lib/timex_datalink_client/eeprom/list_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe TimexDatalinkClient::Protocol3::Eeprom::List do
+describe TimexDatalinkClient::Eeprom::List do
   let(:list_entry) { "Muffler Bearings" }
   let(:priority) { 0 }
 

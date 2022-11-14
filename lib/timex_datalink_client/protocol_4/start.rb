c4fb567546dacf0ff5cc30a351ed11a2500e3ec0
Maxwell Pray <synthead@gmail.com>
2022-11-13 19:31:49 -0800

    Add Protocol4::Start.

---
+++ lib/timex_datalink_client/protocol_4/start.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "timex_datalink_client/helpers/crc_packets_wrapper"
+
+class TimexDatalinkClient
+  class Protocol4
+    class Start
+      prepend Helpers::CrcPacketsWrapper
+
+      CPACKET_START = [0x20, 0x00, 0x00, 0x04]
+
+      # Compile packets for data start command.
+      #
+      # @return [Array<Array<Integer>>] Two-dimensional array of integers that represent bytes.
+      def packets
+        [CPACKET_START]
+      end
+    end
+  end
+end

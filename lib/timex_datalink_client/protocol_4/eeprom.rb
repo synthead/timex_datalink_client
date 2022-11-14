d51db6f57484b242e78225da863c09b562743bc4
Maxwell Pray <synthead@gmail.com>
2022-11-13 21:06:00 -0800

    Add protocol 4 EEPROM classes.

---
+++ lib/timex_datalink_client/protocol_4/eeprom.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require "timex_datalink_client/helpers/cpacket_paginator"
+require "timex_datalink_client/helpers/crc_packets_wrapper"
+
+class TimexDatalinkClient
+  class Protocol4
+    class Eeprom
+      include Helpers::CpacketPaginator
+      prepend Helpers::CrcPacketsWrapper
+
+      CPACKET_CLEAR = [0x93, 0x01]
+      CPACKET_SECT = [0x90, 0x01]
+      CPACKET_DATA = [0x91, 0x01]
+      CPACKET_END = [0x92, 0x01]
+
+      CPACKET_DATA_LENGTH = 32
+      START_ADDRESS = 0x0236
+      APPOINTMENT_NO_NOTIFICATION = 0xff
+
+      attr_accessor :appointments, :anniversaries, :phone_numbers, :lists, :appointment_notification
+
+      # Create an Eeprom instance.
+      #
+      # @param appointments [Array<Appointment>] Appointments to be added to EEPROM data.
+      # @param anniversaries [Array<Anniversary>] Anniversaries to be added to EEPROM data.
+      # @param phone_numbers [Array<PhoneNumber>] Phone numbers to be added to EEPROM data.
+      # @param lists [Array<List>] Lists to be added to EEPROM data.
+      # @param appointment_notification [Integer] Appointment notification (intervals of 15 minutes, 255 for no
+      #   notification)
+      # @return [Eeprom] Eeprom instance.
+      def initialize(appointments: [], anniversaries: [], phone_numbers: [], lists: [], appointment_notification: APPOINTMENT_NO_NOTIFICATION)
+        @appointments = appointments
+        @anniversaries = anniversaries
+        @phone_numbers = phone_numbers
+        @lists = lists
+        @appointment_notification = appointment_notification
+      end
+
+      # Compile packets for EEPROM data.
+      #
+      # @return [Array<Array<Integer>>] Two-dimensional array of integers that represent bytes.
+      def packets
+        [CPACKET_CLEAR, header] + payloads + [CPACKET_END]
+      end
+
+      private
+
+      def header
+        [
+          CPACKET_SECT,
+          payloads.length,
+          items_addresses,
+          items_lengths,
+          earliest_appointment_year,
+          appointment_notification
+        ].flatten
+      end
+
+      def payloads
+        paginate_cpackets(header: CPACKET_DATA, length: CPACKET_DATA_LENGTH, cpackets: all_packets)
+      end
+
+      def all_items
+        [appointments, lists, phone_numbers, anniversaries]
+      end
+
+      def all_packets
+        all_items.flatten.map(&:packet).flatten
+      end
+
+      def items_addresses
+        address = START_ADDRESS
+
+        all_items.each_with_object([]) do |items, addresses|
+          addresses.concat(address.divmod(256))
+
+          address += items.sum { |item| item.packet.length }
+        end
+      end
+
+      def items_lengths
+        all_items.map(&:length)
+      end
+
+      def earliest_appointment_year
+        earliest_appointment = appointments.min_by(&:time)
+
+        return 0 unless earliest_appointment
+
+        earliest_appointment.time.year % 100
+      end
+    end
+  end
+end
---
+++ spec/lib/timex_datalink_client/protocol_4/eeprom/list_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe TimexDatalinkClient::Protocol4::Eeprom::List do
+  let(:list_entry) { "Muffler Bearings" }
+  let(:priority) { 0 }
+
+  let(:list) do
+    described_class.new(
+      list_entry: list_entry,
+      priority: priority
+    )
+  end
+
+  describe "#packet", :length_packet do
+    subject(:packet) { list.packet }
+
+    it_behaves_like "a length-prefixed packet", [
+      0x00, 0x96, 0xf7, 0x3c, 0x95, 0xb3, 0x91, 0x8b, 0xa3, 0x6c, 0xd2, 0x05, 0x71, 0x3f
+    ]
+
+    context "when priority is 3" do
+      let(:priority) { 3 }
+
+      it_behaves_like "a length-prefixed packet", [
+        0x03, 0x96, 0xf7, 0x3c, 0x95, 0xb3, 0x91, 0x8b, 0xa3, 0x6c, 0xd2, 0x05, 0x71, 0x3f
+      ]
+    end
+
+    context "when list_entry is \"Headlight Fluid with More than 31 Characters\"" do
+      let(:list_entry) { "Headlight Fluid with More than 31 Characters" }
+
+      it_behaves_like "a length-prefixed packet", [
+        0x00, 0x91, 0xa3, 0x34, 0x95, 0x04, 0x45, 0x1d, 0xf9, 0x54, 0x9e, 0xd4, 0x90, 0xa0, 0xd4, 0x45, 0xa4, 0x85,
+        0x6d, 0x0e, 0xd9, 0x45, 0xca, 0x45, 0xfe
+      ]
+    end
+
+    context "when list_entry is \";@_|<>[]\"" do
+      let(:list_entry) { ";@_|<>[]" }
+
+      it_behaves_like "a length-prefixed packet", [
+        0x00, 0x36, 0xae, 0xef, 0x7c, 0xef, 0x93, 0x3f
+      ]
+    end
+
+    context "when list_entry is \"~with~invalid~characters\"" do
+      let(:list_entry) { "~with~invalid~characters" }
+
+      it_behaves_like "a length-prefixed packet", [
+        0x00, 0x24, 0x28, 0x75, 0x11, 0x29, 0x5d, 0x9f, 0x52, 0x49, 0x0d, 0xc9, 0x44, 0xca, 0xa6, 0x30, 0x9d, 0xb3,
+        0x71, 0x3f
+      ]
+    end
+  end
+end

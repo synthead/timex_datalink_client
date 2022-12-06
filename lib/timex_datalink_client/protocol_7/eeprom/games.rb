1874248ff2a22fa3d70c658e4d1a16df0385deda
Maxwell Pray <synthead@gmail.com>
2022-12-05 21:03:34 -0800

    Add Protocol7::Eeprom::Games.

---
+++ lib/timex_datalink_client/protocol_7/eeprom/games.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require "timex_datalink_client/helpers/four_byte_formatter"
+
+class TimexDatalinkClient
+  class Protocol7
+    class Eeprom
+      class Games
+        include Helpers::FourByteFormatter
+
+        COUNTDOWN_TIMER_SECONDS_DEFAULT = 60
+
+        COUNTDOWN_TIMER_SOUND_DEFAULT = 0x062
+        MUSIC_TIME_KEEPER_SOUND_DEFAULT = 0x062
+
+        PACKETS_TERMINATOR = 0x02
+
+        attr_accessor :memory_game_enabled, :fortune_teller_enabled, :countdown_timer_enabled, :countdown_timer_seconds,
+          :countdown_timer_sound, :mind_reader_enabled, :music_time_keeper_enabled, :music_time_keeper_sound,
+          :morse_code_practice_enabled, :treasure_hunter_enabled, :rhythm_rhyme_buster_enabled, :stop_watch_enabled,
+          :red_light_green_light_enabled
+
+        # Create a Games instance.
+        #
+        # @param memory_game_enabled [Boolean] Toggle memory game.
+        # @param fortune_teller_enabled [Boolean] Toggle fortune teller.
+        # @param countdown_timer_enabled [Boolean] Toggle countdown timer.
+        # @param countdown_timer_seconds [Integer] Duration for countdown timer in seconds.
+        # @param countdown_timer_sound [Integer] Sound for countdown timer.
+        # @param mind_reader_enabled [Boolean] Toggle mind reader.
+        # @param music_time_keeper_enabled [Boolean] Toggle music time keeper.
+        # @param music_time_keeper_sound [Integer] Sound for music time keeper.
+        # @param morse_code_practice_enabled [Boolean] Toggle Morse code practice.
+        # @param treasure_hunter_enabled [Boolean] Toggle treasure hunter.
+        # @param rhythm_rhyme_buster_enabled [Boolean] Toggle rhythm & rhyme buster.
+        # @param stop_watch_enabled [Boolean] Toggle stop watch.
+        # @param red_light_green_light_enabled [Boolean] Toggle red light, green light.
+        # @return [Games] Games instance.
+        def initialize(
+          memory_game_enabled: false,
+          fortune_teller_enabled: false,
+          countdown_timer_enabled: false,
+          countdown_timer_seconds: COUNTDOWN_TIMER_SECONDS_DEFAULT,
+          countdown_timer_sound: COUNTDOWN_TIMER_SOUND_DEFAULT,
+          mind_reader_enabled: false,
+          music_time_keeper_enabled: false,
+          music_time_keeper_sound: MUSIC_TIME_KEEPER_SOUND_DEFAULT,
+          morse_code_practice_enabled: false,
+          treasure_hunter_enabled: false,
+          rhythm_rhyme_buster_enabled: false,
+          stop_watch_enabled: false,
+          red_light_green_light_enabled: false
+        )
+          @memory_game_enabled = memory_game_enabled
+          @fortune_teller_enabled = fortune_teller_enabled
+          @countdown_timer_enabled = countdown_timer_enabled
+          @countdown_timer_seconds = countdown_timer_seconds
+          @countdown_timer_sound = countdown_timer_sound
+          @mind_reader_enabled = mind_reader_enabled
+          @music_time_keeper_enabled = music_time_keeper_enabled
+          @music_time_keeper_sound = music_time_keeper_sound
+          @morse_code_practice_enabled = morse_code_practice_enabled
+          @treasure_hunter_enabled = treasure_hunter_enabled
+          @rhythm_rhyme_buster_enabled = rhythm_rhyme_buster_enabled
+          @stop_watch_enabled = stop_watch_enabled
+          @red_light_green_light_enabled = red_light_green_light_enabled
+        end
+
+        # Compile data for games.
+        #
+        # @return [Array<Integer>] Compiled data for games.
+        def packets
+          [
+            enabled_games,
+            countdown_timer_time,
+            sounds,
+            PACKETS_TERMINATOR
+          ].flatten
+        end
+
+        private
+
+        def enabled_games
+          bitmask = games.each_with_index.sum do |game, game_index|
+            game ? 1 << game_index : 0
+          end
+
+          bitmask.divmod(256).reverse
+        end
+
+        def countdown_timer_time
+          (countdown_timer_seconds * 10).divmod(256).reverse
+        end
+
+        def sounds
+          sounds_extra_packet = four_byte_format_for(
+            [
+              [music_time_keeper_sound],
+              [countdown_timer_sound],
+              []
+            ]
+          )
+
+          sounds_extra_packet.first(10)
+        end
+
+        def games
+          [
+            memory_game_enabled,
+            fortune_teller_enabled,
+            countdown_timer_enabled,
+            mind_reader_enabled,
+            music_time_keeper_enabled,
+            morse_code_practice_enabled,
+            treasure_hunter_enabled,
+            rhythm_rhyme_buster_enabled,
+            stop_watch_enabled,
+            red_light_green_light_enabled
+          ]
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/timex_datalink_client/protocol_7/eeprom/games_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe TimexDatalinkClient::Protocol7::Eeprom::Games do
+  let(:memory_game_enabled) { false }
+  let(:fortune_teller_enabled) { false }
+  let(:countdown_timer_enabled) { false }
+  let(:countdown_timer_seconds) { 60 }
+  let(:countdown_timer_sound) { 0x062 }
+  let(:mind_reader_enabled) { false }
+  let(:music_time_keeper_enabled) { false }
+  let(:music_time_keeper_sound) { 0x062 }
+  let(:morse_code_practice_enabled) { false }
+  let(:treasure_hunter_enabled) { false }
+  let(:rhythm_rhyme_buster_enabled) { false }
+  let(:stop_watch_enabled) { false }
+  let(:red_light_green_light_enabled) { false }
+
+  let(:sounds) do
+    described_class.new(
+      memory_game_enabled: memory_game_enabled,
+      fortune_teller_enabled: fortune_teller_enabled,
+      countdown_timer_enabled: countdown_timer_enabled,
+      countdown_timer_seconds: countdown_timer_seconds,
+      countdown_timer_sound: countdown_timer_sound,
+      mind_reader_enabled: mind_reader_enabled,
+      music_time_keeper_enabled: music_time_keeper_enabled,
+      music_time_keeper_sound: music_time_keeper_sound,
+      morse_code_practice_enabled: morse_code_practice_enabled,
+      treasure_hunter_enabled: treasure_hunter_enabled,
+      rhythm_rhyme_buster_enabled: rhythm_rhyme_buster_enabled,
+      stop_watch_enabled: stop_watch_enabled,
+      red_light_green_light_enabled: red_light_green_light_enabled
+    )
+  end
+
+  describe "#packets" do
+    subject(:packets) { sounds.packets }
+
+    it { should eq([0x00, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+
+    context "when memory_game_enabled is true" do
+      let(:memory_game_enabled) { true }
+
+      it { should eq([0x01, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when fortune_teller_enabled is true" do
+      let(:fortune_teller_enabled) { true }
+
+      it { should eq([0x02, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when countdown_timer_enabled is true" do
+      let(:countdown_timer_enabled) { true }
+
+      it { should eq([0x04, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when countdown_timer_seconds is 20" do
+      let(:countdown_timer_seconds) { 20 }
+
+      it { should eq([0x00, 0x00, 0xc8, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when countdown_timer_sound is 0x069" do
+      let(:countdown_timer_sound) { 0x069 }
+
+      it { should eq([0x00, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x69, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when mind_reader_enabled is true" do
+      let(:mind_reader_enabled) { true }
+
+      it { should eq([0x08, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when music_time_keeper_enabled is true" do
+      let(:music_time_keeper_enabled) { true }
+
+      it { should eq([0x10, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when music_time_keeper_sound is 0x064" do
+      let(:music_time_keeper_sound) { 0x064 }
+
+      it { should eq([0x00, 0x00, 0x58, 0x02, 0x30, 0x64, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when morse_code_practice_enabled is true" do
+      let(:morse_code_practice_enabled) { true }
+
+      it { should eq([0x20, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when treasure_hunter_enabled is true" do
+      let(:treasure_hunter_enabled) { true }
+
+      it { should eq([0x40, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when rhythm_rhyme_buster_enabled is true" do
+      let(:rhythm_rhyme_buster_enabled) { true }
+
+      it { should eq([0x80, 0x00, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when stop_watch_enabled is true" do
+      let(:stop_watch_enabled) { true }
+
+      it { should eq([0x00, 0x01, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+
+    context "when red_light_green_light_enabled is true" do
+      let(:red_light_green_light_enabled) { true }
+
+      it { should eq([0x00, 0x02, 0x58, 0x02, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x30, 0x62, 0xfe, 0x00, 0x00, 0x02]) }
+    end
+  end
+end

6b18f64d12629d5cd150863b7dbd3f66bf0494ae
Maxwell Pray <synthead@gmail.com>
2022-12-23 16:56:40 -0600

    Add validation for Protocol9::Time's zone value.

--- lib/timex_datalink_client/protocol_9/time.rb
+++ lib/timex_datalink_client/protocol_9/time.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
+require "active_model"
+
 require "timex_datalink_client/helpers/crc_packets_wrapper"
 
 class TimexDatalinkClient
   class Protocol9
     class Time
+      include ActiveModel::Validations
       prepend Helpers::CrcPacketsWrapper
 
       CPACKET_TIME = [0x30]
 
       attr_accessor :zone, :is_24h, :time
+
+      validates :zone, inclusion: {
+        in: 1..2,
+        message: "%{value} is invalid!  Valid zones are 1..2."
+      }
 
       # Create a Time instance.
       #
@@ -25,8 +33,11 @@ class TimexDatalinkClient
 
       # Compile packets for a time.
       #
+      # @raise [ActiveModel::ValidationError] One or more model values are invalid.
       # @return [Array<Array<Integer>>] Two-dimensional array of integers that represent bytes.
       def packets
+        validate!
+
         [
           [
             CPACKET_TIME,

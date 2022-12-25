326db1e1c47f0c57b062d52f9e97105d3ebd5db8
Maxwell Pray <synthead@gmail.com>
2022-12-24 20:32:22 -0600

    Validate days based on month for Protocol1::Alarm.

--- spec/lib/timex_datalink_client/protocol_1/alarm_spec.rb
+++ spec/lib/timex_datalink_client/protocol_1/alarm_spec.rb
@@ -104,13 +104,24 @@ describe TimexDatalinkClient::Protocol1::Alarm do
       ]
     end
 
-    context "when month is 12" do
-      let(:month) { 12 }
+    context "when month is 2" do
+      let(:month) { 2 }
 
       it_behaves_like "CRC-wrapped packets", [
-        [0x50, 0x01, 0x00, 0x00, 0x0c, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x00],
+        [0x50, 0x01, 0x00, 0x00, 0x02, 0x00, 0x0a, 0x15, 0x0a, 0x1b, 0x16, 0x24, 0x01, 0x24, 0x00],
         [0x70, 0x00, 0x62, 0x00]
       ]
+
+      context "when day is 35" do
+        let(:day) { 35 }
+
+        it do
+          expect { packets }.to raise_error(
+            ActiveModel::ValidationError,
+            "Validation failed: Day 35 is invalid for month 2!  Valid days are 1..29 and nil when month is 2."
+          )
+        end
+      end
     end
 
     context "when month is 0" do

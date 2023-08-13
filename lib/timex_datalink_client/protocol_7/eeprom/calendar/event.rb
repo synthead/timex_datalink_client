9f6ebb2c2c276e477c37bbd05a2463a752a1a96e
Maxwell Pray <synthead@gmail.com>
2023-08-12 17:21:35 -0700

    Add Calendar::Event time validation for protocol 7.

--- lib/timex_datalink_client/protocol_7/eeprom/calendar/event.rb
+++ lib/timex_datalink_client/protocol_7/eeprom/calendar/event.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_model"
+
 require "timex_datalink_client/helpers/lsb_msb_formatter"
 
 class TimexDatalinkClient
@@ -7,16 +9,23 @@ class TimexDatalinkClient
     class Eeprom
       class Calendar
         class Event
+          include ActiveModel::Validations
           include Helpers::LsbMsbFormatter
 
           FIVE_MINUTES_SECONDS = 300
 
-          attr_accessor :time, :phrase
+          validates :time, comparison: {
+            greater_than_or_equal_to: :device_time,
+            message: "%{value} must be greater than device time!"
+          }
+
+          attr_accessor :time, :phrase, :device_time
 
           # Create an Event instance.
           #
           # @param time [::Time] Time of event.
           # @param phrase [Array<Integer>] Phrase for event.
+          # @raise [ActiveModel::ValidationError] One or more model values are invalid.
           # @return [Event] Event instance.
           def initialize(time:, phrase:)
             @time = time
@@ -24,6 +33,10 @@ class TimexDatalinkClient
           end
 
           def time_formatted(device_time)
+            @device_time = device_time
+
+            validate!
+
             device_time_midnight = Time.new(device_time.year, device_time.month, device_time.day)
             seconds = (time - device_time_midnight).to_i
             five_minutes = seconds / FIVE_MINUTES_SECONDS
